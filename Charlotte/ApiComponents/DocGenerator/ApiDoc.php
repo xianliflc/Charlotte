27325bb1ac38dc7557b6f74eb653c260a765ce11
xian.li <kagurahun@gmail.com>
2018-04-13 15:25:18 -0400

    updates: generate specific documentation format for api projects

--- Charlotte/ApiComponents/DocGenerator/ApiDoc.php
+++ Charlotte/ApiComponents/DocGenerator/ApiDoc.php
@@ -13,4 +13,28 @@ class ApiDoc Extends Doc {
     public function exportTo($format, $options) {
         // TODO: export doc to with certain format and store in designed folder
     }
+
+    public function refine() {
+        parent::refine();
+        $new_data = array();
+        foreach ($this->data as $group => $group_data) {
+
+            $new_data[$group] = array('group'=>array(), 'endpoints' => array());
+
+            foreach($group_data as $class) {
+                $new_data[$group]['group'] = array_merge($new_data[$group]['group'], $class['class']['comment']['group']);
+                foreach($class['methods'] as $method) {
+                    if (array_key_exists('RequestUrl', $method['comment'])) {
+                        foreach($method['comment']['RequestUrl'] as $url) {
+                            $new_data[$group]['endpoints'][$url] = $method['comment'];
+                            $new_data[$group]['endpoints'][$url]['RequestUrl'] = $url;
+                        }
+                        
+                    }
+                }
+            }
+
+        }
+        $this->data = $new_data;
+    }
 }
